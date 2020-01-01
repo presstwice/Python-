67d455a990d97e44d74233d61f1c6bac00ee09a9
doug oliver <douglas@shellseed.com>
2020-01-01 20:07:45 +1000

    added wip for python tut 1

---
+++ hello/numbers.py
@@ -0,0 +1,13 @@
+from math import *
+
+my_num = -5
+print(ceil(3.7))
+
+# Commands for numbers
+# Always follows a print(
+    #round - rounds a number to the nearest
+    #floor - rounds a number down
+    #ceil - rounds a number up
+    #sqrt - square roots a number 
+
+    

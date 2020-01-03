479d9c118cc178a159519334699a306adc1dc823
doug oliver <douglas@shellseed.com>
2020-01-03 12:44:47 +1000

    Added various app tutorials from pytut1

---
+++ pytut1/Student_App/Student.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+
+# this defines what a student is 
---
+++ pytut1/Student_App/app.py
@@ -0,0 +1,6 @@
+# using the student class you can create a student object
+
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 3.8, True)

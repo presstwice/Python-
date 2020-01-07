cd88ef1b48ecbebd6934e12449b26b3b0e9093f9
doug oliver <douglas@shellseed.com>
2020-01-07 21:33:22 +1000

    Added second version of text classifier following reccomended tensorflow specs

--- TextClassifier/app.py
+++ TextClassifier/app.py
@@ -1,3 +1,5 @@
+# Version of tutorial relied of out of date num.py 
+
 import tensorflow as td
 from tensorflow import keras
 import numpy as np
@@ -6,7 +8,7 @@ data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 
-print(train_data[0])
+#print(train_data[0])
 
 word_index = data.get_word_index()
 
@@ -24,7 +26,7 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-print(decode_review(test_data[0]))
+# print(decode_review(test_data[0]))
 
 # model down here
 
@@ -35,3 +37,17 @@ model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
 
 model.summary()
+
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+
+results = model.evaulate(test_data, test_labels)
+
+print(results)

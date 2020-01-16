8d8ec9ab3765e5cb055d9e53140d99e1b8f05c5b
doug oliver <douglas@shellseed.com>
2020-01-16 23:23:13 +1000

    Added count by quiz while loop

---
+++ udemy_Intro_to_python/count_by.py
@@ -0,0 +1,14 @@
+start_num = 4 # provide some start number
+end_num = 101 # provide some end number that you stop when you hit
+count_by = 3 # provide some number to count by
+
+# write a while loop that uses break_num as the ongoing number to
+
+# check againgst end_num 
+
+break_num = start_num 
+
+while break_num < end_num:
+    break_num += count_by
+
+print(break_num)

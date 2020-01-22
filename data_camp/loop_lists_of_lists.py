e861b952b59b913977836c11502114f510f96eb5
doug oliver <douglas@shellseed.com>
2020-01-22 10:48:13 +1000

    loop indexing excercises

---
+++ data_camp/loop_lists_of_lists.py
@@ -0,0 +1,22 @@
+
+# Lists iteration 
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for room, area in house:
+    print("the " + str(room) + " is " + str(area) + " sqm")
+
+# dictionary looping iterations 
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for k, v in europe.items():
+    print("the capital of " + k + " is " + v)

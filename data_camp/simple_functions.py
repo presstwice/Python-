8d171d907d10134114200fbcec5ed21f06492c91
doug oliver <douglas@shellseed.com>
2020-01-24 20:32:40 +1000

    Finished function and parameter excercise

--- data_camp/simple_functions.py
+++ data_camp/simple_functions.py
@@ -9,3 +9,15 @@ def shout():
 
 # Call shout
 shout()
+
+# Define shout with the parameter, word
+def shout(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout('congratulations')

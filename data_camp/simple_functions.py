a72903afcea5647f4dc3faa4c280b6a912cb7ead
doug oliver <douglas@shellseed.com>
2020-01-26 13:29:51 +1000

    added nested functions and scope excersies

--- data_camp/simple_functions.py
+++ data_camp/simple_functions.py
@@ -1,3 +1,5 @@
+# excercise 1
+
 # Define the function shout
 def shout():
     """Print a string with three exclamation marks"""
@@ -10,6 +12,8 @@ def shout():
 # Call shout
 shout()
 
+# excercise 2
+
 # Define shout with the parameter, word
 def shout(word):
     """Print a string with three exclamation marks"""
@@ -21,3 +25,20 @@ def shout(word):
 
 # Call shout with the string 'congratulations'
 shout('congratulations')
+
+# excercise 3
+
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + "!!!"
+
+    # Replace print with return
+    return shout_word
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)

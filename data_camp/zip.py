31c433243831ef588eb74033821739e6e03ae96e
doug oliver <douglas@shellseed.com>
2020-01-28 13:48:37 +1000

    Added description of zip

--- data_camp/zip.py
+++ data_camp/zip.py
@@ -1,3 +1,5 @@
+# zip accepts an arbitary number of itterables and returns a itterators of tuples 
+
 avengers = ['hawkeye', 'iron man', 'thor', 'quicksilver']
 names = ['barton', 'stark', 'odinson', 'maximoff']
 

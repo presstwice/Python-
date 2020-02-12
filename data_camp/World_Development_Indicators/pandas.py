77d0762bb3a375940fb1e5d67b7e359ff1b73f2b
doug oliver <douglas@shellseed.com>
2020-02-12 19:55:16 +1000

    Fixed the problem with read_csv by updating pandas in the tensorflow conda environment and reseting vscode, yay

--- data_camp/World_Development_Indicators/pandas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("World_Developement_Indicators/worldbank_Data.csv")
-
-print(df.head())
---
+++ data_camp/World_Development_Indicators/world_bank.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('World_Development_Indicators/worldbank_Data.csv')
+
+
+print(df.head())

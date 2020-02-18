d28e14c0b06ef9fc98447d2cf0259d67a7bc5ba4
doug oliver <douglas@shellseed.com>
2020-02-18 21:43:11 +1000

    Working on classifying iris species

--- data_camp/iris_data/2d_iris_view.1.py
+++ data_camp/iris_data/2d_iris_view.1.py
@@ -1,7 +1,12 @@
-# Load the data
-from sklearn.datasets import load_iris
-iris = load_iris()
+# Import modules
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-from matplotlib import pyplot as plt
+# Load the data
+iris = pd.read_csv('iris_data/iris.txt', sep=',', 
+                               names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
 
 print(iris.head())
+
+print(iris['species'].describe())
